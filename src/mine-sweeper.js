5f994be13fc4816c7a806712f6d0e4caba86716b
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-12-11 10:55:15 +0100

    refactor: duplicate code + complexity in progress

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,18 +1,28 @@
 function mineSweeper(playerPick, bombLocations) {
+  let result = '';
   let board = '';
+  let boardMessage = '';
   console.log(bombLocations);
-  if (playerPick[4] === true) {
-    board = '+-+-+-+\n| | | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| | | |\n+-+-+-+\n\n[Sandbox 3x3] BOOM! – Game Over.';
-  } else if (playerPick[0] === true) {
-    board = '+-+-+-+\n|X| | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n\n[Sandbox 3x3] BOOM! – Game Over.';
-  } else if (playerPick[6] === true) {
-    board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n|X| | |\n+-+-+-+\n\n[Sandbox 3x3] BOOM! – Game Over.';
-  } else {
-    board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n\n[Sandbox 3x3] Game created';
+
+  if (playerPick.length === 0) {
+    boardMessage = '[Sandbox 3x3] Game created';
+    board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n\n';
   }
 
-  console.log(board);
-  return board;
+  if (playerPick[4] === true) {
+    boardMessage = '[Sandbox 3x3] BOOM! – Game Over.';
+    board = '+-+-+-+\n| | | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| | | |\n+-+-+-+\n\n';
+  } else if (playerPick[0] === true) {
+    boardMessage = '[Sandbox 3x3] BOOM! – Game Over.';
+    board = '+-+-+-+\n|X| | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n\n';
+  } else if (playerPick[6] === true) {
+    boardMessage = '[Sandbox 3x3] BOOM! – Game Over.';
+    board = '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n|X| | |\n+-+-+-+\n\n';
+  }
+  result = board + boardMessage;
+
+  console.log(result);
+  return result;
 }
 
 module.exports = mineSweeper;
